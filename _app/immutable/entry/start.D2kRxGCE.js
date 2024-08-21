22d2bccc05ad2b2813b1809d45d01ac0b08ed385
Adam Plouff <adamplouff@gmail.com>
2024-08-21 14:55:49 -0400

    Updates

---
+++ _app/immutable/entry/start.D2kRxGCE.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.De_YmINJ.js";export{t as start};
--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<h1>Adam Plouff</h1>
-<p>Archmage of simplicity</p>
-
-<p>
-  <a href="http://battleaxe.co" target="_blank" rel="noopener noreferrer">Battle Axe</a>
-</p>
