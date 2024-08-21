af7154758c364c005b6762a16a797dab2ff45473
Adam Plouff <adamplouff@gmail.com>
2024-08-21 14:02:07 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<div>
+  <slot/>
+</div>
+
+<style>
+  :global(html) {
+    font-family: 'Space mono', monospace;
+    font-size: 16px;
+    background-color: black;
+    color: #eee;
+  }
+  :global(a) {
+    color: #f00;
+  }
+</style>
